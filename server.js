b6c92a2802636f884141a8aae9c5e474fef0c86a
arjuna122 <femasaryapratama26@gmail.com>
2026-05-02 02:52:07 +0700

    update

--- server.js
+++ server.js
@@ -223,3 +223,23 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+// ================= Admin2 =================
+app.put("/api/admin/users/:id/role", verifyToken, verifyAdmin, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    user.role = "admin";
+
+    await user.save();
+
+    res.json({ message: "User promoted to admin" });
+
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
